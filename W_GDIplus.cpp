57d083f18f8448b6f0fe7e4ead51c1e4cfd44ba6
HatsuneMiku <999Hatsune@gmail.com>
2012-11-05 11:47:18 +0900

    LoadImageFromResource (bug)

--- W_GDIplus.cpp
+++ W_GDIplus.cpp
@@ -1,25 +1,74 @@
 /*
-  W_GDIplus.c
+  W_GDIplus.cpp
 
   rc W_GDIplus.rc
-  cl W_GDIplus.c W_GDIplus.res kernel32.lib user32.lib gdi32.lib -DUNICODE
-  late binding LoadLibrary("gdiplus.dll") instead of link gdiplus.lib
+  cl W_GDIplus.cpp W_GDIplus.res kernel32.lib user32.lib ole32.lib gdi32.lib \
+    gdiplus.lib -DUNICODE
 */
 
 #include <windows.h>
+#include <gdiplus.h>
 
 #define CLASS_NAME L"W_GDIPLUS"
 #define APP_NAME L"W_GDIPLUS"
 #define WINDOW_WIDTH 512
 #define WINDOW_HEIGHT 768
 
+Gdiplus::Bitmap *LoadImageFromResource(HINSTANCE hinst,
+  LPCTSTR name, LPCTSTR typ)
+{
+  HRSRC hres = FindResource(hinst, name, typ);
+  if(!hres) return NULL;
+  DWORD siz = SizeofResource(hinst, hres);
+  if(!siz) return NULL;
+  HGLOBAL hdata = LoadResource(hinst, hres);
+  if(!hdata) return NULL;
+  const void *pdata = LockResource(hdata);
+  if(!pdata) return NULL;
+  HGLOBAL hbuf = GlobalAlloc(GMEM_MOVEABLE, siz);
+  if(hbuf){
+    void *pbuf = GlobalLock(hbuf);
+    if(pbuf){
+      CopyMemory(pbuf, pdata, siz);
+      IStream *s = NULL;
+      if(CreateStreamOnHGlobal(hbuf, FALSE, &s) == S_OK){
+MessageBox(NULL, L"stream", APP_NAME, MB_OK);
+        Gdiplus::Bitmap *bitmap = Gdiplus::Bitmap::FromStream(s);
+        s->Release();
+if(!bitmap) MessageBox(NULL, L"bitmap==NULL", APP_NAME, MB_OK);
+        if(bitmap){
+          if(bitmap->GetLastStatus() == Gdiplus::Ok){
+            GlobalUnlock(hbuf);
+            GlobalFree(hbuf);
+            return bitmap;
+          }
+          delete bitmap;
+        }
+      }
+      GlobalUnlock(hbuf);
+    }
+    GlobalFree(hbuf);
+    hbuf = NULL;
+  }
+  return NULL;
+}
+
 LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
 {
+  static Gdiplus::Bitmap *bitmap = NULL;
+  // HINSTANCE hinst = ((LPCREATESTRUCT)lparam)->hInstance; // WM_CREATE only
+  HINSTANCE hinst = (HINSTANCE)GetWindowLong(hwnd, GWL_HINSTANCE);
   switch(msg){
+  case WM_CREATE:
+    bitmap = LoadImageFromResource(hinst, L"png01", L"IMAGE");
+    if(!bitmap) MessageBox(hwnd, L"cannot load resource", APP_NAME,
+      MB_OK | MB_ICONEXCLAMATION);
+    return FALSE;
   case WM_LBUTTONDOWN:
-    MessageBox(hwnd, L"WM_LBUTTON", APP_NAME, MB_OK);
+    MessageBox(hwnd, L"WM_LBUTTON", APP_NAME, MB_OK | MB_ICONINFORMATION);
     return FALSE;
   case WM_DESTROY:
+    if(bitmap){ delete bitmap; bitmap = NULL; }
     PostQuitMessage(0);
     return FALSE;
   }
@@ -47,25 +96,23 @@ BOOL RegCls(HINSTANCE hinst)
 
 int WINAPI WinMain(HINSTANCE hinst, HINSTANCE hprev, LPSTR cmd, int ncmd)
 {
-  if(!RegCls(hinst)){
-    return 1;
-  }else{
-    int x = (GetSystemMetrics(SM_CXSCREEN) - WINDOW_WIDTH) / 2;
-    int y = (GetSystemMetrics(SM_CYSCREEN) - WINDOW_HEIGHT) / 2;
-    HWND hwnd = CreateWindow(CLASS_NAME, APP_NAME,
-      WS_OVERLAPPEDWINDOW | WS_VISIBLE,
-      x, y, WINDOW_WIDTH, WINDOW_HEIGHT, NULL, NULL, hinst, NULL);
-    if(!hwnd){
-      return 1;
-    }else{
-      MSG msg;
-      ShowWindow(hwnd, SW_SHOW);
-      UpdateWindow(hwnd);
-      while(GetMessage(&msg, NULL, 0, 0) > 0){
-        TranslateMessage(&msg);
-        DispatchMessage(&msg);
-      }
-      return 0;
-    }
+  if(!RegCls(hinst)) return 1;
+  int x = (GetSystemMetrics(SM_CXSCREEN) - WINDOW_WIDTH) / 2;
+  int y = (GetSystemMetrics(SM_CYSCREEN) - WINDOW_HEIGHT) / 2;
+  HWND hwnd = CreateWindow(CLASS_NAME, APP_NAME,
+    WS_OVERLAPPEDWINDOW | WS_VISIBLE,
+    x, y, WINDOW_WIDTH, WINDOW_HEIGHT, NULL, NULL, hinst, NULL);
+  if(!hwnd) return 1;
+  ShowWindow(hwnd, ncmd);
+  UpdateWindow(hwnd);
+  ULONG_PTR token;
+  Gdiplus::GdiplusStartupInput gsi;
+  if(Gdiplus::GdiplusStartup(&token, &gsi, NULL) != Gdiplus::Ok) return 1;
+  MSG msg;
+  while(GetMessage(&msg, NULL, 0, 0) > 0){
+    TranslateMessage(&msg);
+    DispatchMessage(&msg);
   }
+  Gdiplus::GdiplusShutdown(token);
+  return msg.wParam;
 }
